9d7139593f98cdd7aadfc446c6d1f7710aac0c5c
firuzyusupov <firuzyusupov@gmail.com>
2026-09-19 22:58:59 +0500

    fix: node-version 22, remove old .ts scripts, clean workflows

--- scripts/build.ts
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
-const version = pkg.version;
-
-console.log(`Building QasriCRM v${version} for production...`);
--- scripts/pre-build.ts
+++
@@ -1,1 +0,0 @@
-console.log('Pre-build: Setting up staging environment...');
